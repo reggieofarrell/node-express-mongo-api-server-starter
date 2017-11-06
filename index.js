a88f19559fdb227c74ba1207d325c6dadcfddb4b
Reggie O'Farrell <reggie.ofarrell@gmail.com>
2017-11-06 12:56:46 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// main starting point of app
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser'); // used to parse requests
+const morgan = require('morgan'); // logging framework
+const app = express();
+const router = require('./router');
+const mongoose = require('mongoose');
+
+//DB setup
+mongoose.connect('mongodb://localhost:auth/auth');
+
+//App setup
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));  // parse all requests as json
+router(app);
+
+//Server Setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening on: ', port);
